a73eeeffb7481b60d4f91e41679137f4038342eb
vava53344-lgtm <vava53344@gmail.com>
2026-08-02 18:14:57 +0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,9 @@ using namespace geode::prelude;
 class TextBoxPopup : public geode::Popup {
 protected:
     TextInput* m_textInput = nullptr;
-    CCLabelBMFont* m_resultLabel = nullptr;
 
     bool init() {
-        if (!Popup::init(280.f, 180.f)) {
+        if (!Popup::init(280.f, 160.f)) {
             return false;
         }
 
@@ -26,17 +25,12 @@ protected:
         // Hint text
         auto hint = CCLabelBMFont::create("Enter text below:", "bigFont.fnt");
         hint->setScale(0.45f);
-        m_mainLayer->addChildAtPosition(hint, Anchor::Center, ccp(0, 40));
+        m_mainLayer->addChildAtPosition(hint, Anchor::Center, ccp(0, 30));
 
         // The text box itself (single-line input with background)
         m_textInput = TextInput::create(200.f, "Type here...");
         m_textInput->setMaxCharCount(64);
-        m_mainLayer->addChildAtPosition(m_textInput, Anchor::Center, ccp(0, 10));
-
-        // Result label (empty by default)
-        m_resultLabel = CCLabelBMFont::create("", "chatFont.fnt");
-        m_resultLabel->setScale(0.5f);
-        m_mainLayer->addChildAtPosition(m_resultLabel, Anchor::Center, ccp(0, -25));
+        m_mainLayer->addChildAtPosition(m_textInput, Anchor::Center, ccp(0, -5));
 
         // Confirm button
         auto okSprite = ButtonSprite::create("OK");
@@ -46,7 +40,7 @@ protected:
 
         auto menu = CCMenu::create();
         menu->addChild(okBtn);
-        m_mainLayer->addChildAtPosition(menu, Anchor::Center, ccp(0, -55));
+        m_mainLayer->addChildAtPosition(menu, Anchor::Center, ccp(0, -45));
 
         return true;
     }
@@ -54,14 +48,12 @@ protected:
     void onConfirm(CCObject*) {
         auto text = m_textInput->getString();
 
-        if (text.empty()) {
-            m_resultLabel->setString("You entered nothing!");
-            m_resultLabel->setColor({255, 90, 90});
-        } else {
-            std::string result = "You entered: " + std::string(text);
-            m_resultLabel->setString(result.c_str());
-            m_resultLabel->setColor({120, 255, 120});
-        }
+        std::string result = text.empty()
+            ? std::string("You entered nothing!")
+            : ("You entered: " + std::string(text));
+
+        // This shows the native GD alert style (same as "Move to Top" etc.)
+        FLAlertLayer::create("Result", result.c_str(), "OK")->show();
     }
 
 public:
