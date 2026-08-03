b8095c1848b852c36f22c9f1610bbb0b67313035
vava53344-lgtm <vava53344@gmail.com>
2026-08-03 22:53:52 +0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 using namespace geode::prelude;
 
 $execute {
-    MotionBlurState::get()->init();
     if (auto mod = Mod::get()) {
         MotionBlurState::get()->m_enabled = mod->getSettingValue<bool>("enabled");
         MotionBlurState::get()->m_strength = static_cast<float>(mod->getSettingValue<double>("strength"));
